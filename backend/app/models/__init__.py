23b1c4efc3b814e291f8116ccc2faf3800039a3d
Xudong Zhao <info.xd.ovo@gmail.com>
2025-03-03 19:53:58 +0800

    initialize Flask-Migrate with global variable in init_db function

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 db = SQLAlchemy()
-migrate = Migrate(app, db)  # 初始化 Flask-Migrate
+migrate = None  # 初始化 Flask-Migrate
 
 def init_db(app):
     db.init_app(app)
+    global migrate  # 使用全局变量
+    migrate = Migrate(app, db)
     with app.app_context():
         db.create_all()
